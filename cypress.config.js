8e98c5a62e3fcbfc48544f35d1c2ba7f0ee0ea23
How What <charlau30@gmail.com>
2022-08-06 08:10:22 +0200

    fix: cypress testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-
+    video: false,
+    screenshotOnRunFailure: false,
+    pageLoadTimeout: 180000,
     setupNodeEvents() {
       // implement node event listeners here
     },
